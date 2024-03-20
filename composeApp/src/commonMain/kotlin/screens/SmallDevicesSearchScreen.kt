ecb2ff0b7155fff9885db5c48d750a6be51b3eb6
Muhammad Atif <ma2669637@gmail.com>
2024-03-20 16:52:52 +0500

    added search screen

--- composeApp/src/commonMain/kotlin/screens/SmallDevicesSearchScreen.kt
+++ composeApp/src/commonMain/kotlin/screens/SmallDevicesSearchScreen.kt
@@ -2,8 +2,10 @@ package screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,8 +19,11 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DockedSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -34,8 +39,10 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.traversalIndex
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import api.ApiClass
 import com.seiko.imageloader.rememberImagePainter
 import kotlinx.coroutines.launch
@@ -46,90 +53,257 @@ import model.SearchUserDataClass
 fun SmallDeviceSearchScreenUI() {
     var text by rememberSaveable { mutableStateOf("") }
     var active by rememberSaveable { mutableStateOf(false) }
-/*    var user by remember { mutableStateOf<SearchUserDataClass?>(null) }
+    var user by remember { mutableStateOf<SearchUserDataClass?>(null) }
     val scope = rememberCoroutineScope()
-    scope.launch {
-        user = ApiClass().getUser("seabdulbasit")
-    }*/
+
 
     Column(Modifier.fillMaxSize().semantics { isTraversalGroup = true }) {
-        DockedSearchBar(
-            modifier = Modifier.padding(start = 12.dp, end = 12.dp, top = 36.dp)
-                .align(Alignment.CenterHorizontally)
-                .semantics { traversalIndex = -1f },
-            query = text,
-            onQueryChange = { text = it },
-            onSearch = { active = false },
-            active = active,
-            onActiveChange = { active = it },
+        TextField(
+            modifier = Modifier.fillMaxWidth().padding(start = 12.dp, end = 12.dp, top = 36.dp)
+                .align(Alignment.CenterHorizontally),
+            value = text,
+            onValueChange = { text = it },
             placeholder = { Text("Search a user", color = Color.White) },
             leadingIcon = {
-                Icon(
-                    Icons.Default.Search,
-                    contentDescription = null,
-                    tint = Color.White
-                )
+                IconButton(onClick = {
+                    scope.launch {
+                        user = ApiClass().getUser(text)
+                    }
+                }) {
+                    Icon(
+                        Icons.Default.Search,
+                        contentDescription = null,
+                        tint = Color.White
+                    )
+                }
+
             },
             trailingIcon = {
-                if (active) {
-                    Icon(
-                        Icons.Default.Close,
-                        contentDescription = null,
-                        tint = Color.White,
-                        modifier = Modifier.clickable {
-                            if (text.isNotEmpty()) {
-                                text = ""
-                            } else {
-                                active = false
-                            }
-
-                        })
+                if (text.isNotEmpty()){
+                    IconButton(onClick = {
+                        text = ""
+                    }){
+                        Icon(
+                            Icons.Default.Close,
+                            contentDescription = null,
+                            tint = Color.White,
+                            modifier = Modifier
+                        )
+                    }
                 }
+
+
             },
-            colors = SearchBarDefaults.colors(
-                containerColor = Color(0xFF0a2b34),
-                inputFieldColors = TextFieldDefaults.colors(
-                    cursorColor = Color.White,
-                    unfocusedTextColor = Color.White,
-                    disabledLeadingIconColor = Color.White,
-                    disabledTrailingIconColor = Color.White,
-                    focusedLeadingIconColor = Color.White,
-                    focusedTrailingIconColor = Color.White,
-                    disabledTextColor = Color.White,
-                    focusedTextColor = Color.White
-                )
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color(0xFF0a2b34),
+                unfocusedContainerColor = Color(0xFF0a2b34),
+                disabledContainerColor = Color(0xFF0a2b34),
+                cursorColor = Color.White,
+                unfocusedTextColor = Color.White,
+                disabledLeadingIconColor = Color.White,
+                disabledTrailingIconColor = Color.White,
+                focusedLeadingIconColor = Color.White,
+                focusedTrailingIconColor = Color.White,
+                disabledTextColor = Color.White,
+                focusedTextColor = Color.White,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent
+
             ),
-        ) {
-            repeat(4) { idx ->
+            shape = RoundedCornerShape(50)
+        )
+        user?.let {
+            Box(modifier = Modifier.fillMaxSize().padding(top = 12.dp)) {
+
+                Card(
+                    modifier = Modifier.fillMaxSize().padding(top = 81.dp),
+                    shape = RoundedCornerShape(18.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color(0xFF0d2f3a)
+                    )
+                ) {
+                    Column(
+                        modifier = Modifier.fillMaxWidth().padding(top = 72.dp),
+                        verticalArrangement = Arrangement.spacedBy(12.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Row(
+                            modifier = Modifier.padding(horizontal = 18.dp),
+                            horizontalArrangement = Arrangement.spacedBy(18.dp) // Aligns children to start and end
+                        ) {
+                            Text(
+                                "Name",
+                                modifier = Modifier,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                            Text(
+                                it.name,
+                                fontFamily = FontFamily.Default,
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                        }
+
+                        Row(
+                            modifier = Modifier.padding(horizontal = 18.dp),
+                            horizontalArrangement = Arrangement.spacedBy(18.dp) // Aligns children to start and end
+                        ) {
+                            Text(
+                                "Repos",
+                                modifier = Modifier,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                            Text(
+                                it.public_repos.toString(),
+                                fontFamily = FontFamily.Default,
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                        }
+
+                        Row(
+                            modifier = Modifier.padding(horizontal = 18.dp),
+                            horizontalArrangement = Arrangement.spacedBy(18.dp) // Aligns children to start and end
+                        ) {
+                            Text(
+                                "Gists",
+                                modifier = Modifier,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                            Text(
+                                it.public_gists.toString(),
+                                fontFamily = FontFamily.Default,
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                        }
+                        Row(
+                            modifier = Modifier.padding(horizontal = 18.dp),
+                            horizontalArrangement = Arrangement.spacedBy(18.dp) // Aligns children to start and end
+                        ) {
+                            Text(
+                                "Followers",
+                                modifier = Modifier,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                            Text(
+                                it.followers.toString(),
+                                fontFamily = FontFamily.Default,
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                        }
+                        Row(
+                            modifier = Modifier.padding(horizontal = 18.dp),
+                            horizontalArrangement = Arrangement.spacedBy(18.dp) // Aligns children to start and end
+                        ) {
+                            Text(
+                                "Following",
+                                modifier = Modifier,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                            Text(
+                                it.following.toString(),
+                                fontFamily = FontFamily.Default,
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                        }
+
+                        Row(
+                            modifier = Modifier.padding(horizontal = 18.dp),
+                            horizontalArrangement = Arrangement.spacedBy(18.dp) // Aligns children to start and end
+                        ) {
+                            Text(
+                                "Created",
+                                modifier = Modifier,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                            Text(
+                                it.created_at.substring(0,10),
+                                fontFamily = FontFamily.Default,
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                        }
+
+                        Row(
+                            modifier = Modifier.padding(horizontal = 18.dp),
+                            horizontalArrangement = Arrangement.spacedBy(18.dp) // Aligns children to start and end
+                        ) {
+                            Text(
+                                "Updated",
+                                modifier = Modifier,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                            Text(
+                                it.updated_at.substring(0,10),
+                                fontFamily = FontFamily.Default,
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 16.sp,
+                                lineHeight = 24.sp,
+                                letterSpacing = 0.5.sp,
+                                color = Color(0xFF8ecede)
+                            )
+                        }
+                    }
 
 
-                Text(
-                    "Abdul Basit",
-                    fontWeight = FontWeight.Bold,
-                    color = Color(0xFF6a9fac),
-                    modifier = Modifier.padding(start = 36.dp)
-                )
+                }
 
+                Card(
+                    modifier = Modifier.size(150.dp).align(Alignment.TopCenter),
+                    shape = RoundedCornerShape(100)
+                ) {
+                    Image(
+                        rememberImagePainter("https://avatars.githubusercontent.com/u/33172684?v=4"),
+                        null,
+                        contentScale = ContentScale.Crop
+                    )
 
-
+                }
             }
         }
-
-
-        Box(modifier = Modifier.fillMaxSize().padding(top = 12.dp)){
-
-            Card(modifier = Modifier.fillMaxSize().padding(top = 81.dp), shape = RoundedCornerShape(18.dp), colors = CardDefaults.cardColors(
-                containerColor = Color(0xFF0d2f3a)
-            )){
-
-            }
-
-            Card(modifier = Modifier.size(150.dp).align(Alignment.TopCenter), shape = RoundedCornerShape(100)){
-                Image(rememberImagePainter("https://avatars.githubusercontent.com/u/33172684?v=4"),null, contentScale = ContentScale.Crop)
-
-            }
-        }
-
 
 
     }
