210c2af190f8c9e53d62d9a94e2be681072e47aa
Muhammad Atif <ma2669637@gmail.com>
2024-03-25 16:07:25 +0500

    added close functionality on alt+f4 in desktop

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,9 +1,29 @@
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.isAltPressed
+import androidx.compose.ui.input.key.key
+import androidx.compose.ui.window.ApplicationScope
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import api.ApiClass
+import kotlinx.coroutines.launch
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "Github Users") {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Github Users",
+        onKeyEvent = {
+            when  {
+                it.isAltPressed && it.key == Key.F4 -> {
+                    exitApplication()
+                }
+
+            }
+            true
+
+        }) {
+
         App()
     }
 }
